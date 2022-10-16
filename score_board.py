a931852894c6d28e524326677481ab25851e148d
Keri Nguyen <k4zy007@outlook.com>
2022-10-16 17:23:33 +0700

    Update score_board.py

--- score_board.py
+++ score_board.py
@@ -8,6 +8,10 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        # mở file high_score.txt lấy dữ liệu cho high_score
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
+        file.close()
         self.color("black")
         self.penup()
         self.hideturtle()
@@ -15,7 +19,18 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            # ghi dữ liệu high_score vào file high_score.txt
+            with open("high_score.txt", mode="w") as file:
+                file.write(str(self.high_score))
+            file.close()
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
@@ -23,5 +38,4 @@ class ScoreBoard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
